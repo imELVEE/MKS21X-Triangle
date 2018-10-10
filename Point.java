8ac396653921ca7deccd33a5d2d5fa5e15dd787f
Alvi Aziz <aaziz00@stuy.edu>
2018-10-10 16:44:57 -0400

    made all of Point

--- Point.java
+++ Point.java
@@ -2,7 +2,7 @@ public class Point{
 	private double x;
 	private double y;
 
-	public Point(x, y) {
+	public Point(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
@@ -15,4 +15,15 @@ public class Point{
 		return y;
 	}
 
+	public double distanceTo(Point toP) {
+		return Math.sqrt( Math.pow(toP.x - x , 2) + Math.pow(toP.y - y , 2) );
+	}
+
+	public double distance(Point froP, Point toP) {
+		return Math.sqrt( Math.pow(toP.x - froP.x , 2) + Math.pow(toP.y - froP.y , 2) );
+	}
+
+	public String toString() {
+		return (x + ", " + y);
+	}
 }
