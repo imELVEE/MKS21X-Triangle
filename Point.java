32c8fd4f55495dc76d250ba81e7039f13ffbfcd3
Alvi Aziz <aaziz00@stuy.edu>
2018-10-10 14:52:57 -0400

    made the files and some code

---
+++ Point.java
@@ -0,0 +1,18 @@
+public class Point{
+	private double x;
+	private double y;
+
+	public Point(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+}
